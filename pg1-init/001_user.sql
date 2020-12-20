f172aed4c33560b87fbae034330bbd89a0d161d1
vraisamis <quick.sorting.arg@gmail.com>
2020-12-20 16:10:47 +0900

    add db

---
+++ pg1-init/001_user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+  id integer
+  , name varchar(100)
+  , atk integer
+  , def integer
+);
+
+INSERT INTO users VALUES
+(1, 'makai no hanayome', 13850, 13750)
+, (2, 'amai seiiki', 22785, 17640)
+;
---
+++ pg2-init/001_user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users(
+  id integer
+  , name varchar(100)
+  , vo integer
+  , da integer
+  , vi integer
+);
+
+INSERT INTO users VALUES
+(1, 'unmei no machibito', 3780, 4588, 7080)
+, (2, 'syukuhuku no syutora-ru', 3880, 7124, 4688)
+;
